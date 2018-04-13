b49eae5495217e496cdb4343a295c7f52916bccf
Дмитрий Карих <the.dr.hax@gmail.com>
2018-04-14 02:34:22 +0300

    AVD: Enable keyboard support

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -4,4 +4,9 @@ echo n | $ANDROID_HOME/tools/bin/avdmanager create avd \
     -k "system-images;${TARGET};${TAG};${ABI}" \
     -n ${NAME} -b ${ABI} -g ${TAG}
 
+(
+  # Enable keyboard support in QEMU (for VNC)
+  echo 'hw.keyboard = true';
+) >> /root/.android/avd/Docker.avd/config.ini
+
 exec $*
